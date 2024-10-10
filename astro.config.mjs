2ba54c604f129e0b47bb0914c2e0b10d6e059b1b
Cenfos <camilo.rguez@gmail.com>
2024-10-10 22:16:22 +0200

    Nuevos estilos globales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  integrations: [mdx()],
 });
